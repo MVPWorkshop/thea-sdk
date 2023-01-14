ef1c60d66a0caad896d8afe3f202b399ead43452
Kaliman <akalimancevic@gmail.com>
2023-01-15 00:15:09 +0100

    fix: code coverage 100%

--- test/modules/recover.spec.ts
+++ test/modules/recover.spec.ts
@@ -14,6 +14,7 @@ import {
 import { PRIVATE_KEY } from "../mocks";
 import * as shared from "../../src/modules/shared";
 import BaseTokenManager_ABI from "../../src/abi/BaseTokenManager_ABI.json";
+import { formatBytes32String } from "@ethersproject/strings";
 
 jest.mock("../../src/modules/shared", () => {
 	return {
@@ -63,6 +64,15 @@ describe("Recover", () => {
 		rating: BigNumber.from(5),
 		btVintage: BigNumber.from(2020)
 	};
+
+	const getFeatureValueTransaction: Partial<{ vintageValue: BigNumber; sdgValue: BigNumber; ratingValue: BigNumber }> =
+		{
+			vintageValue: BigNumber.from(2021),
+			sdgValue: BigNumber.from(2),
+			ratingValue: BigNumber.from(5)
+		};
+	const getCharacteristicsBytesTransaction =
+		"0x00000000000000000000000000000000000000000000000000000000000007e3000000000000000000000000000000000000000000000000000000000000000f0000000000000000000000000000000000000000000000000000000000000002";
 
 	const mockContract: Partial<IBaseTokenManagerContract> = {
 		recover: jest.fn().mockResolvedValue(contractTransaction as ContractTransaction),
@@ -176,6 +186,50 @@ describe("Recover", () => {
 			const result = recover.extractInfoFromEvent([event as Event]);
 			expect(result.id).toBe("1");
 			expect(result.amount).toBe("1000");
+		});
+	});
+
+	describe("calculateBaseTokensAmounts", () => {
+		it("should return btVintage, sdg, vintage and rating amounts", async () => {
+			const id = "1";
+			const amount = "1000";
+			const unitAmount = BigNumber.from(amount).mul(10);
+			const baseCharacteristics = {
+				vintage: BigNumber.from(2020),
+				sdgsCount: BigNumber.from(1),
+				rating: BigNumber.from(4)
+			};
+
+			const getFeatureValueTxPromise = Promise.resolve(
+				getFeatureValueTransaction as { vintageValue: BigNumber; sdgValue: BigNumber; ratingValue: BigNumber }
+			);
+			const getFeatureValueSpy = jest.spyOn(recover, "getFeatureValue").mockReturnValue(getFeatureValueTxPromise);
+			const result = await recover.calculateBaseTokensAmounts(id, amount, baseCharacteristics);
+
+			expect(getFeatureValueSpy).toHaveBeenCalledWith(id);
+			expect(result.btVintage).toEqual(unitAmount);
+			expect(result.sdg).toEqual(unitAmount);
+			expect(result.vintage).toEqual(unitAmount);
+			expect(result.rating).toEqual(unitAmount);
+		});
+	});
+
+	describe("getFeatureValue", () => {
+		it("should return sdgValue, vintageValue and ratingValue", async () => {
+			const id = "1";
+			const keys = [formatBytes32String("vintage"), formatBytes32String("sdgs_count"), formatBytes32String("rating")];
+			const decodedValues = [BigNumber.from(2019), BigNumber.from(15), BigNumber.from(2)];
+
+			const getCharacteristicsBytesTxPromise = Promise.resolve(getCharacteristicsBytesTransaction as string);
+			const getCharacteristicsBytesTxSpy = jest
+				.spyOn(recover.registry, "getCharacteristicsBytes")
+				.mockReturnValue(getCharacteristicsBytesTxPromise);
+			const result = await recover.getFeatureValue(id);
+
+			expect(getCharacteristicsBytesTxSpy).toHaveBeenCalledWith(id, keys);
+			expect(result.vintageValue).toEqual(decodedValues[0]);
+			expect(result.sdgValue).toEqual(decodedValues[1]);
+			expect(result.ratingValue).toEqual(decodedValues[2]);
 		});
 	});
 });
